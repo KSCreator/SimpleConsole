3447ec04d3d6fd33a12f4a146f2fac9db5f25695
Keshav Sahu <keshavpiproud999@gmail.com>
2021-09-09 19:54:21 +0530

    removed Sample.cpp

--- Sample.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char **argv)
-{
-	std::cout << "argument count : " << argc << std::endl;
-	for(int i = 0; i < argc; ++i)
-		std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
-}
