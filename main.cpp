06292e40eedaad22d79efbf794f1b3d2c0c81534
Keshav Sahu <keshavpiproud999@gmail.com>
2021-09-09 19:51:42 +0530

    Now program and arguments will start after the -p option.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
+#include <cstring>
 #include <QApplication>
 #include <QProcess>
 #include <QDir>
-
 #include "Console.hpp"
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2)
-        return -1;
-    QApplication a(argc, argv);
-    QString program(argv[1]);
-    QStringList arguments;
-    for(int i = 2; i < argc; ++i)
+    int qt_argc = 0;
+    while(++qt_argc < argc
+          && std::strcmp(argv[qt_argc],"-p"))
     {
-        arguments.push_back(argv[i]);
+        //
     }
-    Console w(program,arguments);
-    w.show();
+    if(qt_argc == argc || qt_argc == argc-1)    //if no -p or it was last token
+    {
+        qDebug("no program specified");
+        return -1;
+    }
+    QScopedArrayPointer<char*> qt_args(new char*[qt_argc]);
+    const int qt_argc_original = qt_argc;
+    QApplication a(qt_argc, qt_args.data());    //qt argc may changed
+    QString program_name(argv[qt_argc_original+1]);
+    QStringList user_args;
+    for(int i = qt_argc_original + 2; i < argc; ++i)
+    {
+        user_args.push_back(argv[i]);
+    }
+    Console console(program_name,user_args);
+    console.show();
     //process.waitForFinished();
     return a.exec();
 }
